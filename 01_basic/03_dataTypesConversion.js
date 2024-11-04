44a102ffbcf7237ddbe7d92ca16b9617c1500243
Vijay <vy1202952@gmail.com>
2024-11-04 09:26:46 +0530

    operation in data types conversions

--- 01_basic/03_dataTypesConversion.js
+++ 01_basic/03_dataTypesConversion.js
@@ -29,5 +29,22 @@
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+// ######################## Operations #######################
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
