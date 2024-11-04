0aa992ad395a1ab6299dc838f747bf633280e4da
Vijay <vy1202952@gmail.com>
2024-11-04 08:35:35 +0530

    basic of datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3);  we can't use alert as we are using node js and alert is available in browser
+
+console.log(3
+    +
+    3); // code readability always should be high
+
+console.log("Vijay");
+
+let name = "vijay";
+let age = 24;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned yet
+// symbol=> unique
+
+//object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
