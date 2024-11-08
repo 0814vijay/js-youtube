70b4d84e4946b0fdc9e34372ca99f5e7722c4c60
Vijay <vy1202952@gmail.com>
2024-11-08 09:16:25 +0530

    date and time

---
+++ 01_basic/08_dates.js
@@ -0,0 +1,21 @@
+let myDate = new Date();
+// console.log(myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getHours());
+// console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+
+let createdDate = new Date(2024, 10, 8);
+// console.log(createdDate.toDateString());
+// let x = createdDate.toLocaleString('en-IN').split(',');
+// console.log(x[0]);
+// console.log(createdDate.toLocaleDateString('en-IN'));
+// console.log(createdDate.toLocaleString('en-IN'));
+
+console.log(myDate.toLocaleString())
+
