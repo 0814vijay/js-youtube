0d5423a8aa9d52e1551364cd9f03f0d731d90576
Vijay <vy1202952@gmail.com>
2024-11-15 10:21:15 +0530

    object destructuring

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -13,6 +13,17 @@ const obj2 = { 3: "a", 4: "b" }
 // console.log(Object.values(obj1))
 // console.log(Object.entries(obj1));
 
-console.log(obj1.hasOwnProperty('isLoggedIn'));
-console.log(obj1.hasOwnProperty('1'));
+// console.log(obj1.hasOwnProperty('isLoggedIn'));
+// console.log(obj1.hasOwnProperty('1'));
+
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    instructor: "hitesh"
+}
+const { instructor: ins } = course;
+// we can rename value as per our requirement , if we rename that value then we need to rename value only
+// we can't use previous value in this case 
+// console.log(instructor);
+console.log(ins);
 
