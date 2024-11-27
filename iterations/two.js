be12dd5b116c52352f9f87ad480daefe09474794
Vijay <vy1202952@gmail.com>
2024-11-27 09:36:47 +0530

    Basic of while and do while loop

---
+++ iterations/two.js
@@ -0,0 +1,22 @@
+// let i =0;
+// while(i<=10){
+//     console.log(i);
+//     i=i+2
+    
+// }
+
+// let myArray=['Sam',"harry","Joe"]
+// let i =0;
+// while(i<myArray.length){
+//     console.log(myArray[i]);
+//     i++
+    
+// }
+
+let score=11;
+do{
+console.log(score);
+score++
+
+}while(score<=10)
+// in do while loop code run atleast one time
