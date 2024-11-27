78e499e619eb058be26d5ab71977586eb9919432
Vijay <vy1202952@gmail.com>
2024-11-27 09:24:54 +0530

    Basic of for loop

---
+++ iterations/one.js
@@ -0,0 +1,39 @@
+// for (let i = 1; i <= 10; i++) {
+//    for (let j = 1; j <=10; j++) {
+    
+//      console.log(`${i} * ${j} = ${i*j}`)
+//      if(j== 10){
+//         console.log(`table of ${i} completed`)
+//      }
+//    }
+    
+// }
+
+// const myArray=["Sam","John","Harry"];
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     if(index!=0 && index % 2 === 0){
+//         console.log("even string");
+        
+//     }
+//     console.log(element);
+    
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     if(i===5){
+//         console.log("Value 5 detacted");
+//         break
+//     }
+//     console.log(i);
+    
+// }
+// for (let i = 1; i <= 10; i++) {
+//     if(i===5){
+//         console.log("Value 5 detacted");
+//         continue
+//     }
+//     console.log(i);
+    
+// }
